507eac59f645b63e56e85079a73abebd29d394ee
Edmilson Neves <edmilson.neves@akna.com>
2022-02-01 19:50:41 -0300

    change blade

--- resources/views/tenants/home/index.blade.php
+++ resources/views/tenants/home/index.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.layouts.main')
+@extends('tenants.layouts.main')
 
 @section('content')
 <h1>Recursos de tenants</h1>
     <a href="{{ route('company.index') }}">
-        <img src="{{asset(imgs/companies.png)}}" alt="Empresas">
+        <img src="{{asset('imgs/companies.jpg')}}" alt="Empresas">
     </a>
 @endsection
